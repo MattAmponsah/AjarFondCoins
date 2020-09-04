e03faa70e535bb17df6cba5b486050b71080e59a
Matt Amponah <mattamponsah@gmail.com>
2020-09-04 03:50:08 +0000

    spacing

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ tempc = float((Temp1 - 32.0) * 5.0 / 9.0)
 
 tempf = float(Temp1 * 9.0 / 5.0 + 32.0)
 
-if unit == 'F' or unit == 'f':
-    result = print(f"{Temp1}° in Fahrenheit is equivalent to {tempc}° Celsius.")
+if unit == 'F' or unit == 'f':print(f"{Temp1}° in Fahrenheit is equivalent to {tempc}° Celsius.")
 
-elif unit == 'C' or unit == 'c':
-    print(f"{Temp1}° in Celsius is equivalent to {tempf}° Fahrenheit.")
+elif unit == 'C' or unit == 'c':print(f"{Temp1}° in Celsius is equivalent to {tempf}° Fahrenheit.")
 
-else:
-    print(f"Invalid unit({unit}).")
+else:print(f"Invalid unit({unit}).")
